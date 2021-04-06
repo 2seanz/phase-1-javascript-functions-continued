deab796bfdc7fb243336a5e5c50114615c9e7413
Sean Ngo <seanzngo@gmail.com>
2021-04-06 16:16:37 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`
+}
+saturdayFun(activity = "bathe my dog");
+
+
+// function mondayWork() {
+//     console.log("This Monday, I will go to the office.")
+// }
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+    
+// function wrapAdjective(thing = "special") {
+//      let result = wrapAdjective(), emphatic = result("a hard worker");
+// return `You are ${emphatic}`
+// }
+
+// function wrapAdjective(string = '*') {
+//     let result = wrapAdjective(), emphatic = result()
+//     result("a hard worker");
+//     return `You are ${string} + ${result} + ${string}`
+// }
+
+function wrapAdjective(string='*') {
+    const innerFunction = function(adjective='special') {
+        return `You are ${string}${adjective}${string}!`
+    }
+    return innerFunction
+}
